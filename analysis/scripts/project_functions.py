ad17bb58235d69ef9aff9f03958e8ad04b611155
Sebi Unipan <onebread@student.ubc.ca>
2020-11-18 17:36:58 -0800

    Finished milestone 2, there aren't enough grpahs for categorical data. I hope I don't lose marks.

--- analysis/scripts/project_functions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_and_process(directory):
-    
-    df1 = (pd.read_csv(directory)
-          .drop(['Final Weight', 'Education-Num'], axis = 1)
-          .dropna()
-          )
-    return df1
